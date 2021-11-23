cfb38f692851dea7c413546758b0caa3cc9ded9f
sukesan000 <oyama.k.1997@gmail.com>
2021-11-23 10:21:37 +0900

    もろもろ追加

---
+++ studentfrontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Appbar from "./components/Appbar.js";
+import Student from "./components/Student.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Appbar />
+      <Student />
+    </div>
+  );
+}
+
+export default App;
